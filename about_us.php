e8360eb7cbe3ac987912a8c473c179d9f4950f32
liangze <249834096@qq.com>
2017-11-05 22:56:49 +0800

    change about_us.php

--- about_us.php
+++ about_us.php
@@ -55,12 +55,11 @@
                     
                     <div class="section_w590_content">
                     	<p style ="font-size: 15pt; color: maroon"><b><i>We are here!</i></b></p>
-                        <p style ="font-size: 13pt">Republic Polytechnic Library - 9 Woodlands Ave 9, 738964</p>
+                        <p style ="font-size: 13pt">Nanyang Technological Univerisity, Tamarind Halls, BLK 24-10-38, 38 Nanyang Crescent, Singapore 636866</p>
                        <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com.sg/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=9+Woodlands+Ave+9,+738964&amp;aq=&amp;sll=1.443841,103.783722&amp;sspn=0.014973,0.022724&amp;ie=UTF8&amp;hq=&amp;hnear=9+Woodlands+Avenue+9,+738964&amp;t=m&amp;ll=1.453816,103.786039&amp;spn=0.030031,0.036564&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com.sg/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=9+Woodlands+Ave+9,+738964&amp;aq=&amp;sll=1.443841,103.783722&amp;sspn=0.014973,0.022724&amp;ie=UTF8&amp;hq=&amp;hnear=9+Woodlands+Avenue+9,+738964&amp;t=m&amp;ll=1.453816,103.786039&amp;spn=0.030031,0.036564&amp;z=14&amp;iwloc=A" style="color:#0000FF;text-align:left">View Larger Map</a></small>                       
 					   <p style ="font-size: 15pt; color: maroon"><b><i>Contact us!</i></b></p>
-                        <p style ="font-size: 13pt"> +65 63682566</p>
+                        <p style ="font-size: 13pt"> +65 83891612</p>
                         <p style ="font-size: 15pt; color: maroon"><b><i>Like us!</i></b></p>
-                        <iframe width="160" height="30" src="http://www.facebook.com/plugins/like.php?href=http://www.facebook.com/pages/The-Reel-Room/284414581188"></iframe>
                     </div>
                     
                 </div> 
