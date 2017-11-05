c622687e58a07452d0e6dbbdf524cb98ad6cf977
liangze <249834096@qq.com>
2017-11-05 22:11:47 +0800

    ali sql

--- admin/dbFunctions.php
+++ admin/dbFunctions.php
@@ -1,8 +1,9 @@
 <?php
-$HOST = 'localhost';
-$USERNAME = 'root'; 
-$PASSWORD = '110';
-$DB = 'orderOnline';
+$HOST = 'bdm253827620.my3w.com';
+$PORT = 3306;
+$USERNAME = 'bdm253827620';
+$PASSWORD = '64380702Opl';
+$DB = 'bdm253827620_db';
 
 $link = mysqli_connect($HOST,$USERNAME,$PASSWORD,$DB) or die(mysqli_connect_error());
 
